d4f218fb1630112c68ed92e3b039581ba8b0b173
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-04-21 18:33:54 +1000

    refactoring

--- Week01_01.rb
+++ Week01_01.rb
@@ -1,12 +1,21 @@
 # Write a program that reads in a positive integer N and then calculates and displays
 # the sum of the numbers from 1 to N.
 
-puts "Enter a positive integer: "
-STDOUT.flush
-integer = gets.chomp.to_i
-puts "The integer is #{integer}"
-sum = 0
-(integer+1).times do |n|
-	sum = sum + n
+def get_input
+	puts "Enter a positive integer: "
+	STDOUT.flush
+	gets.chomp.to_i
 end
+
+def get_sum (integer)
+	result = 0
+	(integer+1).times do |n|
+		result = result + n
+	end
+	result
+end
+
+integer = get_input
+puts "The integer is #{integer}"
+sum = get_sum(integer)
 puts "The sum is #{sum}"
