498ec9d19a4161a17723910c0bb6cc282782c0ce
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-04-22 21:06:57 +1000

    Second exercise

---
+++ Week01_02.rb
@@ -0,0 +1,22 @@
+# Write a program that reads in a positive integer N and then calculates and displays
+# the sum of the first N odd integers.
+# For example, if N is 4, your program should display the value 16, which is 1 + 3 + 5 + 7.
+
+def get_input
+	puts "Enter a positive integer: "
+	STDOUT.flush
+	gets.chomp.to_i
+end
+
+def get_sum (integer)
+	result = 0
+	(integer+1).times do |n|
+		result = result + n
+	end
+	result
+end
+
+integer = get_input
+puts "The integer is #{integer}"
+sum = get_sum(integer)
+puts "The sum is #{sum}"
