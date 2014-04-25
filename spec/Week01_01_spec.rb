1f1222574dd61a3ddaa6b934e31a75851a627deb
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-04-26 08:56:34 +1000

    Scoped out spec. Need to learn how to simulate keyboard IO

---
+++ spec/Week01_01_spec.rb
@@ -0,0 +1,25 @@
+describe Week01_01 do
+
+  context 'invalid input' do
+    # figure out how to simulate keyboard input
+    # send it a negative integer
+    # send it a zero
+    # send it a float
+    # send it a character string
+    # expect error message (which it currently doesn't do)
+    describe "negative integer" do
+    end
+    describe "zero" do
+    end
+    describe "float" do
+    end
+    describe "character string" do
+    end
+  end
+
+  context 'valid input' do
+    # figure out how to simulate keyboard input
+    # send it a positive integer, say 10
+    # expect output to be 55
+  end
+end
