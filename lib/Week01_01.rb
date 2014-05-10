7fdbb55a32777fc3dfc03a44f79326183183b6ee
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-05-11 09:51:48 +1000

    Refactoring.

--- lib/Week01_01.rb
+++ lib/Week01_01.rb
@@ -1,13 +1,25 @@
 # Write a program that reads in a positive integer N and then calculates and displays
 # the sum of the numbers from 1 to N.
+def get_input
+  puts "Enter a positive integer: "
+  STDOUT.flush
+  gets.chomp
+end
+
+def validate_input(n)
+  # Invalid inputs:
+  raise("Only positive integers are accepted. Not #{n}") if n.to_i == 0 || n.to_i < 0
+  raise("No floats. Only integers are accepted. Not #{n}") unless n.to_f / n.to_i == 1
+end
+
 class Week01_01
   def self.get_sum(n)
-
-    # Invalid inputs:
-    raise("Only positive integers are accepted. Not #{n}") if n.to_i == 0 || n < 0
-    raise("No floats. Only integers are accepted. Not #{n}") unless n / n.to_i == 1
-
     (1..n).inject(0) { |sum, x| sum += x }
-
   end
 end
+
+n = get_input
+puts "The integer is #{n}"
+validate_input(n)
+sum = Week01_01::get_sum(n.to_i)
+puts "The sum is #{sum}"
