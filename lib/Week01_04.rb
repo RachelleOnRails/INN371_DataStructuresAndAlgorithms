919f8051b35d828e6f43eb385c341c912410ef55
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-05-11 17:54:56 +1000

    Refactoring.

--- lib/Week01_04.rb
+++ lib/Week01_04.rb
@@ -11,29 +11,59 @@
 # ? 0
 # The largest value is 42
 
-def get_input
-	puts "Enter a positive integer (or zero to exit): "
-	STDOUT.flush
-	gets.chomp.to_i
+class InvalidInput < StandardError
 end
 
-def find_greatest (inputs)
-	greatest = 0
+class Week01_04
 
-	inputs.each do |n|
-		greatest = n if n > greatest
-	end
-	greatest
+  def self.valid?(inputs)
+    inputs.each do |input|
+      if input.to_s[/^[1-9]\d*$/] == 0
+        return nil
+      end
+    end
+  end
+
+  def self.call(inputs)
+    if valid? inputs
+      greatest = find_greatest (inputs)
+      puts "The greatest is #{greatest}"
+      greatest
+    else
+      raise InvalidInput
+    end
+  end
+
+  def self.get_input
+    puts "Enter a positive integer (or zero to exit): "
+    STDOUT.flush
+    gets.chomp.to_i
+  end
+
+  def self.find_greatest (inputs)
+    greatest = 0
+
+    inputs.each do |n|
+      greatest = n if n > greatest
+    end
+    greatest
+  end
+
 end
 
-inputs = []
-new = -1
+if __FILE__ == $0
+  inputs = []
+  new = -1
 
-loop do
-	new = get_input
-  break if new == 0
-	inputs << new
+  loop do
+    new = Week01_04.get_input
+    break if new == 0
+    inputs << new
+  end
+
+  begin
+    Week01_04.call(inputs)
+  rescue InvalidInput
+    puts "Invalid input. Only positive integers are accepted."
+  end
 end
-
-greatest = find_greatest (inputs)
-puts "The greatest is #{greatest}"
